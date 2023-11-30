0a665f952c59db4cad91f9b9b47ed0f36ef256c3
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-11-30 11:53:16 -0300

    finish all

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import style from '../style/Card.module.css'
+import style from '../style/Card.module.css';
 
 export const Card = ({ id, title, price, description, image }) => {
     return (
-        <div className="col" id={style.contain}>
-            <div className="container h-100 shadow-sm" id={style.containData}>
-                <img src={image} className="card-img-top" alt={title} id={style.img} />
-                <div className="">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">Description: {description}</p>
-                    <div className="clearfix mb-3">
-                        <p className="card-text">$ {price}</p>
-                        <p className="card-text">Sku: 56632215594515</p>
+        <div className={`col ${style.cardContainer}`}>
+            <div className={`container h-100 shadow-sm ${style.card}`}>
+                <img src={image} className={`card-img-top ${style.cardImage}`} alt={title} />
+                <div className={`card-body ${style.cardBody}`}>
+                    <h5 className={`card-title ${style.cardTitle}`}>{title}</h5>
+                    <p className={`card-text ${style.cardDescription}`}>Description: {description}</p>
+                    <div className={`clearfix mb-3 ${style.cardInfo}`}>
+                        <p className={`card-text ${style.cardPrice}`}>$ {price}</p>
+                        <p className={`card-text ${style.cardSku}`}>Sku: 56632215594515</p>
                     </div>
-                    <div className="text-center">
+                    <div className={`text-center ${style.cardButton}`}>
                         <Link to={`details/${id}`}>
-                            <button className="btn btn-primary">Details</button>
+                            <button className={`btn btn-primary`}>Details</button>
                         </Link>
                     </div>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { getAll } from '../api/fetchData'
+import { getAll } from '../api/fetchData';
 import { Navbar } from '../components/Navbar';
 import { Card } from '../components/Card';
 import { Loading } from '../utils/Loading';
+import styles from '../style/Home.module.css';
 
 export const Home = () => {
     const [products, setProducts] = useState([]);
@@ -21,10 +22,10 @@ export const Home = () => {
     }, []);
 
     return (
-        <div>
+        <div className={styles.container}>
             <Navbar />
             <h1 className="text-center">List of Products</h1>
-            <div className="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3  justify-content-center align-items-center">
+            <div className="row row-cols-1 row-cols-s-2 row-cols-m-2 row-cols-lg-3  justify-content-center align-items-center">
                 {products.length !== 0 ? (
                     products.map((product) => (
                         <Card {...product} key={product.id} />
